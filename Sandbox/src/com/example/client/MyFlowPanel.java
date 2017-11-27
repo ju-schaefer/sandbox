8dba794997d12550357ba2cc7b6db4cdc8e30d16
Julian Schäfer <Julian.Schaefer@gmx.de>
2017-11-27 21:57:17 +0100

    Change Border Styles

--- Sandbox/src/com/example/client/MyFlowPanel.java
+++ Sandbox/src/com/example/client/MyFlowPanel.java
@@ -1,11 +1,18 @@
 package com.example.client;
 
+import com.google.gwt.dom.client.Style.BorderStyle;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.ui.FlowPanel;
 
 public class MyFlowPanel extends FlowPanel {
 
 	public MyFlowPanel() {
 		super();
+		
+		getElement().getStyle().setBorderColor("red");
+		getElement().getStyle().setBorderStyle(BorderStyle.DOTTED);
+		getElement().getStyle().setBorderWidth(2, Unit.PX);
+
 	}
 	
 }
