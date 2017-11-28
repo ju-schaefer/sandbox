5cef27382115b882ac34fabb95a6ed0e791cc620
Julian Schäfer <Julian.Schaefer@gmx.de>
2017-11-28 07:20:19 +0100

    Instantiate MyComponentWidget

--- Sandbox/src/com/example/client/Sandbox.java
+++ Sandbox/src/com/example/client/Sandbox.java
@@ -2,6 +2,7 @@ package com.example.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.RootLayoutPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 
 /**
@@ -12,12 +13,16 @@ public class Sandbox implements EntryPoint {
 	@Override
 	public void onModuleLoad() {
 		
-		MyFlowPanel myFlowPanel = new MyFlowPanel();
+		/*MyFlowPanel myFlowPanel = new MyFlowPanel();
 		myFlowPanel.add(new HTML("row a"));
 		myFlowPanel.add(new HTML("row b"));
 		myFlowPanel.add(new HTML("row c"));
 		
-		RootPanel.get().add(myFlowPanel);
+		RootPanel.get().add(myFlowPanel);*/
+		
+		MyCompositeWidget widget = new MyCompositeWidget();
+		
+		RootPanel.get().add(widget);
 	}
 	
 }
