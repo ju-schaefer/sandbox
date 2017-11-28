a52c5ab56c579cd49b4ad5a1c4ffd3a54bac2ae5
Julian Schäfer <Julian.Schaefer@gmx.de>
2017-11-28 07:19:50 +0100

    Add Split Panels Add Content to Split Panels

--- Sandbox/src/com/example/client/MyResizeWidget.java
+++ Sandbox/src/com/example/client/MyResizeWidget.java
@@ -1,11 +1,21 @@
 package com.example.client;
 
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.ResizeComposite;
+import com.google.gwt.user.client.ui.SplitLayoutPanel;
 
 public class MyResizeWidget extends ResizeComposite {
+	
+	private SplitLayoutPanel widget;
 
 	public MyResizeWidget() {
+		widget = new SplitLayoutPanel();
 		
+		widget.addWest(new HTML("navigation"), 128);
+		widget.addNorth(new HTML("list"), 384);
+		widget.add(new HTML("details"));
+		
+		initWidget(widget);
 	}
 	
 }
