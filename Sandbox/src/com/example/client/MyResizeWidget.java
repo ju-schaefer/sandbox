63ba8a06d38e395e45c0c80d38c83fb27cda4474
Julian Schäfer <Julian.Schaefer@gmx.de>
2017-11-28 07:11:23 +0100

    Init ResizeComposite

---
+++ Sandbox/src/com/example/client/MyResizeWidget.java
@@ -0,0 +1,11 @@
+package com.example.client;
+
+import com.google.gwt.user.client.ui.ResizeComposite;
+
+public class MyResizeWidget extends ResizeComposite {
+
+	public MyResizeWidget() {
+		
+	}
+	
+}
